448e85e9ec1c3ec7693b27c38d6ced12542ce977
nathalyamessias <nathalya.messias@iesb.edu.br>
2026-04-10 21:08:51 -0300

    green: implementa funcoes de contagem

--- src/taskManager.js
+++ src/taskManager.js
@@ -88,3 +88,22 @@ export function filterTasks(tasks, status) {
   }
 }
 
+// ------------------------------------------------------------
+// Contagens
+// ------------------------------------------------------------
+
+export function countTasks(tasks) {
+  return tasks.length;
+}
+
+export function countCompleted(tasks) {
+  // Filtra as concluídas e pega o tamanho do array resultante
+  return tasks.filter((task) => task.completed === true).length;
+}
+
+export function countPending(tasks) {
+  // Filtra as não concluídas e pega o tamanho do array resultante
+  return tasks.filter((task) => task.completed === false).length;
+}
+
+
